d833f8924917573bc203de0a82163aa474369fe8
aixx <135753971+naturetraveller@users.noreply.github.com>
2023-06-10 18:36:04 +0200

    Update languagesJS.js

--- languagesJS.js
+++ languagesJS.js
@@ -1,32 +1,11 @@
-// Function to add the 'active' class to the active language link
-function highlightActiveLanguage() {
-  const currentLanguage = document.documentElement.lang;
-  const languageLinks = document.querySelectorAll('.navbar a[href^="index_"]');
-  
-  languageLinks.forEach(link => {
-    const languageCode = link.getAttribute('href').split('_')[1].split('.')[0];
-    if (languageCode === currentLanguage) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
-  });
-}
+// Get the current language from the HTML lang attribute
+const currentLanguage = document.documentElement.lang;
 
-// Function to add the 'active' class to the active page link
-function highlightActivePage() {
-  const currentHash = window.location.hash;
-  const pageLinks = document.querySelectorAll('.navbar a[href^="#"]');
-  
-  pageLinks.forEach(link => {
-    if (link.getAttribute('href') === currentHash) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
-  });
-}
-
-// Call the functions to highlight the active language and active page on page load
-highlightActiveLanguage();
-highlightActivePage();
+// Find the corresponding language button and add the "active" class
+const languageButtons = document.querySelectorAll(".navbar a");
+languageButtons.forEach(button => {
+  const buttonLanguage = button.getAttribute("href").replace(".html", "");
+  if (buttonLanguage === currentLanguage) {
+    button.classList.add("active");
+  }
+});
