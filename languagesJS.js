64446acf7b011c6540d9f6c224681db86adf5a07
aixx <135753971+naturetraveller@users.noreply.github.com>
2023-06-10 18:38:02 +0200

    Update languagesJS.js

--- languagesJS.js
+++ languagesJS.js
@@ -1,11 +1,13 @@
 // Get the current language from the HTML lang attribute
 const currentLanguage = document.documentElement.lang;
 
-// Find the corresponding language button and add the "active" class
+// Find the language buttons and add the "active" class to the current language button
 const languageButtons = document.querySelectorAll(".navbar a");
 languageButtons.forEach(button => {
   const buttonLanguage = button.getAttribute("href").replace(".html", "");
   if (buttonLanguage === currentLanguage) {
     button.classList.add("active");
+  } else {
+    button.classList.remove("active");
   }
 });
